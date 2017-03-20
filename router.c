8ae98c84be9c3d119631ddb9109a22a97ffedfa7
Elaine Yeung <yeung.egs@gmail.com>
2017-03-20 22:26:02 +0000

    Add description and buffer_percent to struct

--- router.c
+++ router.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * router - struct
+ * @va_list: list of args
+ * @char: character string
+ * @int :integer
+ */
 
 int (*router(char fmt))(va_list, char *, int)
 {
@@ -9,7 +15,7 @@ int (*router(char fmt))(va_list, char *, int)
 		{'i', buffer_int},
 		{'d', buffer_int},
 		{'b', buffer_int_b},
-		{'%', buffer_int_per),
+		{'%', buffer_percent},
 		{'\0', NULL}
 	};
 
