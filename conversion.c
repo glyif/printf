07583fbbbfe087a45d741150848bd45daea88f7c
Elaine Yeung <yeung.egs@gmail.com>
2017-03-22 02:27:31 +0000

    Delete trailing whitespace and add function description

--- conversion.c
+++ conversion.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
-
+/**
+ * int_string - checks _printf input
+ * @n: integer
+ * Return: charater string
+ */
 char *int_string(int n)
 {
 	int i;
 	char *s;
-	
+
 	i = 0;
-	
+
 	s = malloc(intlen(n) * sizeof(char));
-	
+
 	if (n == 0)
 	{
 		s[i] = '0';
@@ -17,7 +21,6 @@ char *int_string(int n)
 	{
 		n = _abs(n);
 	}
-		
 
 	while (n != 0)
 	{
@@ -25,12 +28,17 @@ char *int_string(int n)
 		n = n / 10;
 		i++;
 	}
-	
+
 	s[i] = '\0';
-	
+
 	_rev(s);
-	return(s);
+	return (s);
 }
+/**
+ *decimal_binary - checks _printf input
+ *@n: unsigned integer
+ *Return: long integer
+ */
 
 long decimal_binary(unsigned int n)
 {
@@ -51,16 +59,19 @@ long decimal_binary(unsigned int n)
 
 	return (binary);
 }
-
+/**
+ *int_b_string - checks _printf input
+ *@n: unsigned integer
+ *Return: pointer to character
+ */
 char *int_b_string(unsigned int n)
 {
 	int b;
 	char *s;
 
 	b = decimal_binary(n);
-	
-	s = int_string(b); 
-	
-	return(s);
+
+	s = int_string(b);
+
+	return (s);
 }
-	
